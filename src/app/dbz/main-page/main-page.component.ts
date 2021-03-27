5960686a9c5d3031e53d47c90ef3fa6cc0e48750
orlandochr <orlandochr@gmail.com>
2021-03-27 17:13:15 -0300

    Curso Angular, hasta video  58

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder:number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent  {
+
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder:14000
+  }
+
+  // cambiarNombre(event:any){
+  //   console.log(event.target.value);
+  // }
+
+
+  agregar( ){
+   
+
+    console.log(this.nuevo);
+  }
+  
+
+
+
+}
